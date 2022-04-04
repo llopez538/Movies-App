e803205a990a78917944052295d9159156cc9cd3
leonardlopez <masasss.the.end@gmail.com>
2022-04-04 00:37:24 -0400

    viewBinding para inflar las vistas por medio de clases kotlin o Java

--- app/src/main/java/app/mistermobile/mymovies/MainActivity.kt
+++ app/src/main/java/app/mistermobile/mymovies/MainActivity.kt
@@ -2,10 +2,23 @@ package app.mistermobile.mymovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import app.mistermobile.mymovies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val message = findViewById<TextView>(R.id.message)
+        message.text = "Cambiando el texto"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Rotate", "Mensaje de tipo String")
     }
 }
