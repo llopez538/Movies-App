b11e3bfc13bf7bcb3647b795ecfd01020cd25158
leonardlopez <masasss.the.end@gmail.com>
2022-04-06 03:05:39 -0400

    Recibir Valores por medio de una acción setOnClickListener

--- app/src/main/java/app/mistermobile/mymovies/MainActivity.kt
+++ app/src/main/java/app/mistermobile/mymovies/MainActivity.kt
@@ -3,8 +3,14 @@ package app.mistermobile.mymovies
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+
 import app.mistermobile.mymovies.databinding.ActivityMainBinding
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +18,23 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.sendMovie.setOnClickListener {
+            val message = binding.message.text
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        }
 
-        val message = findViewById<TextView>(R.id.message)
-        message.text = "Cambiando el texto"
+        /*val titleMovie = binding.message.text
+        val message = Toast.makeText(this, titleMovie, Toast.LENGTH_LONG)
+
+        val button:Button = findViewById(R.id.sendMovie)
+        button.setOnClickListener {
+            message.show()
+        }*/
+
+        // val message = findViewById<TextView>(R.id.message)
+        // message.text = "Cambiando el texto"
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
